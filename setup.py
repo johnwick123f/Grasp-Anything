969f6f855f63105e01fa3508a56aeb2e62af938c
johnwick123f <128541091+johnwick123f@users.noreply.github.com>
2024-03-20 18:04:25 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GraspAnything',
+    version='0.1',
+    packages=find_packages(),
+    author='Yatharth350',
+    author_email='yatharthsharma350@gmail.com	',
+    description='Estimate Grasps for any Object',
+    url='https://github.com/johnwick123f/Grasp-Anything.git',
+)
